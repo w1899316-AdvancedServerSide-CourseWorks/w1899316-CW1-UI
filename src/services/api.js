c4b61ff8366574ab075bc2446a0bbfa85c7ee3e1
Chamith <chamith.eos@gmail.com>
2025-04-10 12:06:09 +0530

    change the base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/rest-countries/api',
+  baseURL: 'https://cw1-be.autodock.me/rest-countries/api',
   withCredentials: true,
 });
 
