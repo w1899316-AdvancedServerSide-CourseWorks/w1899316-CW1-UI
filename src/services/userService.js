45c5a59a982ee373750fb4a6916279f367c37d82
Chamith <chamith.eos@gmail.com>
2025-04-11 17:03:39 +0530

    user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,7 @@
 import api from './api';
 
 export const getAllUsers = () => api.get('/user/findall');
-export const deleteUser = (id) => api.delete(`/admin/users/${id}`);
+export const deleteUser = (id) =>
+    api.delete('/user/delete/userId', {
+      params: { id },
+    });
